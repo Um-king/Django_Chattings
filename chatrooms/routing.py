29fe32c6907aa452ab79e08d6c8119199871e567
chul <dudcjf3232@gmail.com>
2024-04-15 10:45:36 +0900

    메시지 + 영상통화 기능 구현

--- chatrooms/routing.py
+++ chatrooms/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .consumers import ChatConsumer
+from .consumers import ChatConsumer, CallConsumer
 
 websocket_urlpatterns = [
-	re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi())
+	re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/call/', CallConsumer.as_asgi()),
 ]
